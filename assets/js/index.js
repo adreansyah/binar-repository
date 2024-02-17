91c5e8456d999677b56ec22620fc3ed2e2457ae1
adreansyahaswafi <adreansyah.aswafi@sg-dsa.com>
2024-02-17 12:04:49 +0700

    feat(dom)

---
+++ assets/js/index.js
@@ -0,0 +1,7 @@
+(() => {
+    const text = document.getElementsByClassName('paragraph');
+    console.log(text[0]);
+    text[0].style.color = 'red'
+    text[1].style.color = 'blue'
+    text[2].style.color = 'green'
+})();
